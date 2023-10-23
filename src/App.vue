fca4025860b55051e31d075ea38f5b5776b75b7a
Lukk133 <zielinskil@outlook.com>
2023-10-23 15:27:53 +0200

    Pokemons added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <nav>
 <div class="container text-center mt-5 mb-3">
-  <router-link to="/" class="btn btn-primary">Home</router-link> |
-  <router-link to="/about" class="btn btn-secondary">About</router-link>
+  <router-link to="/" class="btn btn-primary">Names</router-link> |
+  <router-link to="/about" class="btn btn-secondary">Pokemons</router-link>
 </div>
   
 </nav>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,46 @@
-<template>
-  <div class="text-center">This is the about page</div>
+<template >
+  <div class="container">
+    
+      <div v-for="post in posts" :key="post.id">
+        <div> {{ post.entry_number }} {{ post.pokemon_species.name }}</div>
+        
+      </div>
+    </div>
+  
+  <!--<div :hidden="hideData"></div>-->
+  
 </template>
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data() {
+    return {
+      hideData: true,
+      posts: [],
+      task: '',
+      tasks: [
+        {
+          name: 'Peter'
+        }],
+    }
+  },
+  created() {
+    /*można też mounted*/
+    axios
+      .get(`https://pokeapi.co/api/v2/pokedex/2`)
+      .then((response) => {
+        console.log(response.data);
+        this.posts = response.data.pokemon_entries
+      })
+  }
+}
+
+
+
+</script>
+
